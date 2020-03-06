13b545637ff2847aed55bc48816905c4949cf64e
tiagosn01 <tiagosilva0922@gmail.com>
2020-03-06 16:16:58 -0300

    Finalizado desafio 04

--- desafio04/src/App.js
+++ desafio04/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import profile from './assets/profile.jpg'
-import './App.css'
+import Header from './components/Header'
+import PostList from './components/PostList';
+
+import './App.css';
 
 function App() {
-  return <App/>
+  return (
+    <>
+      <Header />
+      <PostList />
+    </>
+  );
 }
 
 export default App;
